1b06fb9fb007b9179fe64631186b7a4e55a2d445
G1Rmister <connorurick@outlook.com>
2025-10-22 12:16:27 -0700

    feat: Add onlyAuthorizedUpdater Modifier

--- src/Weather.sol
+++ src/Weather.sol
@@ -33,4 +33,9 @@ contract WeatherOracle is IDataOracle, Ownable {
 
         // Constants
     uint256 public constant STALE_DATA_THRESHOLD = 4 hours;
+
+    modifier onlyAuthorizedUpdater() {
+        require(authorizedUpdaters[msg.sender] || msg.sender == owner(), "Not authorized updater");
+        _;
+    }
 }
