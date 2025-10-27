b52e057ad58c35c6b268f8071ef8c0dec922e0e5
G1Rmister <connorurick@outlook.com>
2025-10-27 12:32:21 -0700

    feat: Add getDetailedWeatherData External View Function

--- src/Weather.sol
+++ src/Weather.sol
@@ -69,4 +69,11 @@ contract WeatherOracle is IDataOracle, Ownable {
 
         return currentWeather.condition;
     }
+
+        /**
+     * @dev Get detailed weather data
+     */
+    function getDetailedWeatherData() external view returns (WeatherData memory) {
+        return currentWeather;
+    }
 }
