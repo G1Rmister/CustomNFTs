a5d9122a165e7b51a1a8580e61d37000c9720a01
G1Rmister <connorurick@outlook.com>
2025-10-29 11:44:33 -0700

    feat: Add Title Natspec Comment To IDataOracle Interface

--- src/interfaces/IDataOracle.sol
+++ src/interfaces/IDataOracle.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-interface IDataOracle {}
+/**
+ * @title IDataOracle
+ * @dev Interface for data oracle contracts
+ */
+interface IDataOracle {
+}
