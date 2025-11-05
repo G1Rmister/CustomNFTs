33f0faf621fc45c3aae22a1001e6072bf82e7fd7
G1Rmister <connorurick@outlook.com>
2025-11-05 13:42:45 -0800

    feat: Add Necessary Imports

--- src/MetadataRender.sol
+++ src/MetadataRender.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./interfaces/IMetadataRender.sol";
+
 contract MetadataRender {}
--- src/interfaces/IMetadataRender.sol
+++ src/interfaces/IMetadataRender.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-interface IMetadataRenderer {
+interface IMetadataRender {
     struct NFTState {
         uint256 lastWeatherUpdate;
         uint256 lastTimeUpdate;
